374c43b5f8ee16c6e958491822affb523ac364bf
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-03 16:06:20 -0300

    função mostrar senha funcionando

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,37 @@
+//mostrar senha
+function mostrarSenha(num) {
+    senha = document.getElementById(`senha${num}`);
+    imgSenha = document.getElementById(`img-senha${num}`);
+
+    if(senha.type === "password") {
+        senha.type = "text";
+        imgSenha.src = '/public/img/show.png';
+    }
+    else {
+        senha.type = "password";
+        imgSenha.src = '/public/img/hide.png';
+    }
+}
+
+
 let photo = document.getElementById('labelFilme');
 let file = document.getElementById('files-imagem');     
 let modal = false;
 
-file.addEventListener('change', () => {
+// file.addEventListener('change', () => {
 
-    if (file.files.length == 0) {
-        return;
-    }
+//     if (file.files.length == 0) {
+//         return;
+//     }
 
-    let reader = new FileReader();
+//     let reader = new FileReader();
 
-    reader.readAsDataURL(file.files[0]);
+//     reader.readAsDataURL(file.files[0]);
     
 
-    reader.onload = () => {
-        photo.src = reader.result;
-        console.log(reader.result);
-    }
-});
+//     reader.onload = () => {
+//         photo.src = reader.result;
+//         console.log(reader.result);
+//     }
+// });
+
