8572bbeeef2f636077110c46962ba9e1f83cb126
Helengendary <helenapicinin13@gmail.com>
2024-09-03 11:19:24 -0300

    trigger & procedures

--- BancoDados/TRIGGERS.sql
+++ BancoDados/TRIGGERS.sql
@@ -8,6 +8,9 @@ for each row
 begin
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Cliente ', new.Nome, ' ', new.Sobrenome, ' inserido.'), CURDATE());
+    
+    insert into log_historico_clientes values
+    (DEFAULT, new.IDCliente, new.Nome, new.Sobrenome, new.Email, 'Ativo');
 end;
 // DELIMITER ;
 
@@ -18,6 +21,10 @@ for each row
 begin
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Cliente ', old.Nome, ' ', old.Sobrenome, ' removido.'), CURDATE());
+    
+    update log_historico_clientes
+    set Status_Cliente = 'Inativo'
+    where Nome = old.Nome and Sobrenome = old.Sobrenome;
 end;
 // DELIMITER ;
 
@@ -28,6 +35,13 @@ for each row
 begin
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Cliente ', new.Nome, ' ', new.Sobrenome, ' editado.'), CURDATE());
+    
+    update log_historico_clientes
+    set Nome = new.Nome,
+    Sobrenome = new.Sobrenome,
+    Email = new.Email,
+    Status_Cliente = new.Status_Cliente
+    where Nome = old.Nome and Sobrenome = old.Sobrenome;
 end;
 // DELIMITER ;
 
@@ -40,6 +54,9 @@ for each row
 begin
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Filme ', new.Titulo, ' inserido.'), CURDATE());
+    
+    insert into log_historico_filmes values
+    (DEFAULT, new.Titulo, new.Data_Estreia, new.Data_Saida, 'Licença ativa');
 end;
 // DELIMITER ;
 
@@ -50,6 +67,10 @@ for each row
 begin
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Filme ', old.Titulo, ' removido.'), CURDATE());
+    
+	update log_historico_filmes
+    set Status_Filme = 'Licença inativa'
+    where Titulo = old.Titulo;
 end;
 // DELIMITER ;
 
@@ -60,6 +81,13 @@ for each row
 begin
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Filme ', new.Titulo, ' editado.'), CURDATE());
+    
+    update log_historico_filmes
+    set Titulo = new.Titulo,
+    Data_Estreia = new.Data_Estreia,
+    Data_Saida = new.Data_Saida,
+    Status_Filme = new.Status_Filme
+    where Titulo = old.Titulo;
 end;
 // DELIMITER ;
 
@@ -146,9 +174,27 @@ create trigger log_nova_compra
 after insert on Compra
 for each row
 begin
+	DECLARE Nome_Cliente VARCHAR(255);
+    DECLARE Sobrenome_Cliente VARCHAR(255);
+    DECLARE Email VARCHAR(255);
+    DECLARE Valor VARCHAR(255);
+    
 	insert into log_notificacoes values
     (DEFAULT, CONCAT('Nova compra registrada'), CURDATE());
-end;
+    
+
+    SET @Nome_Cliente = (select Nome from Cliente where id = fk_Cliente_id);
+    SET @Sobrenome_Cliente = (select Sobrenome from Cliente where id = fk_Cliente_id);
+    SET @Email = (select Email from Cliente where id = fk_Cliente_id);
+    SET @Valor = (select sum(CN.PrecoIngresso*(TI.porcentagem/100)) from Ingresso I 
+                    inner join Sessao S on S.id = I.fk_Sessao_id
+                    inner join Cinema CN on S.fk_Cinema_id = CN.id
+                    inner join TipoIngresso TI on TI.id = I.fk_TipoIngresso_id
+				where I.fk_Compra_Numero = new.numero);
+    
+    insert into log_historico_compras values
+    (DEFAULT, new.Numero_Compra, @Nome_Cliente, @Sobrenome_Cliente, @Email, @Valor);
+end
 // DELIMITER ;
 
 
@@ -163,3 +209,35 @@ begin
 end;
 // DELIMITER ;
 
+
+-- INGRESSO ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+DELIMITER //
+create trigger log_ingresso_inserido
+after insert on Ingresso
+for each row
+begin
+	DECLARE Numero_Compra int;
+    DECLARE Tipo_Ingresso VARCHAR(20);
+    DECLARE Titulo_Filme VARCHAR(255);
+    DECLARE Numero_Sala VARCHAR(10);
+    DECLARE Tres_D BIT;
+    DECLARE DataHorario DATETIME;
+    
+    SET @Numero_Compra = (select Numero from Compra where id = fk_Compra_Numero);
+    SET @Tipo_Ingresso = (select Tipo from TipoIngresso where id = fk_TipoIngresso_id);
+    SET @Titulo_Filme = (select F.Titulo from Ingresso I
+						 inner join Filme F
+                         on F.id = I.fk_Filme_id);
+    SET @Numero_Sala = (select Salas.NumeroSala from Ingresso I
+						 inner join Sessao S
+                         on S.id = I.fk_Sessao_id
+                         inner join Salas
+                         on Salas.id = S.fk_Salas_id);
+    SET @Tres_D = (select TresD from Sessao where id = fk_Sessao_id);
+    SET @DataHorario = (select DataHorario from Sessao where id = fk_Sessao_id);
+    
+    
+	insert into log_ingressos_vendidos values
+    (DEFAULT, @Numero_Compra, @Tipo_Ingresso, @Titulo_Filme, @Numero_Sala, @Tres_D, @DataHorario);
+end;
+// DELIMITER ;
