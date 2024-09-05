c035d5d34910aa75961abe3d9d4205dbfe3df602
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-05 10:01:07 -0300

    começando a função de editar usuario

--- src/config/bd.js
+++ src/config/bd.js
@@ -1,9 +1,9 @@
 const sequelize = require('sequelize');
 
-const database = new sequelize('MoviefyTicket', 'Moviefy', 'etsps2024401',
+const database = new sequelize('MoviefyTickets', 'Moviefy', 'etsps2024401',
 {
-    dialect: 'mssql', host:'localhost', port: 63650
-    // dialect: 'mssql', host:'localhost', port: 57136
+    // dialect: 'mssql', host:'localhost', port: 63650
+    dialect: 'mssql', host:'localhost', port: 57136
     // dialect: 'mssql', host:'localhost', port: 1433
 });
 
